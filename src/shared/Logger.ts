99c9b1330894ddd343963e1b6b58a24c9449c8ea
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-05-04 20:52:02 -0300

    fix: logger starts at info level

--- src/shared/Logger.ts
+++ src/shared/Logger.ts
@@ -16,7 +16,7 @@ const consoleFormatter = printf(({ stack, level, message, label, timestamp }: { 
 });
 
 const baseLogger = createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  level: 'info',
   defaultMeta: { label: 'main' },
   format: format.combine(format.errors({ stack: true }), format.timestamp({ format: timestampFormatter })),
 });
