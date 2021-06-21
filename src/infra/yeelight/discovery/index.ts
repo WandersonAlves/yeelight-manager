b8f0935e7c8c522dac5cfdc111a191b57f7bb2d3
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-06-21 17:53:24 -0300

    fix: increase discovery time

--- src/infra/yeelight/discovery/index.ts
+++ src/infra/yeelight/discovery/index.ts
@@ -48,7 +48,7 @@ export default class Discovery {
         // this.devices.forEach(d => d.connect());
         client.close();
         resolve(this.devices);
-      }, timeToDiscover ?? 350);
+      }, timeToDiscover ?? 1000);
     });
   }
 }
