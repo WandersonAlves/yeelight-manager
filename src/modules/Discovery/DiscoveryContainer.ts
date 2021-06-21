10a52762ed9c2255aecbacec8c81e9d5f9afbaf6
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-06-21 18:01:45 -0300

    chore: remove express related files

--- src/modules/Discovery/DiscoveryContainer.ts
+++ src/modules/Discovery/DiscoveryContainer.ts
@@ -1,13 +1,9 @@
 import { ContainerModule } from 'inversify';
 import DiscoverDevicesCase from './DiscoverDevices/DiscoverDevicesCase';
-import DiscoverDevicesRouter from './DiscoverDevices/DiscoverDevicesRouter';
 import RetrieveDeviceCase from './RetrieveDevice/RetrieveDeviceCase';
-import RetrieveDeviceRouter from './RetrieveDevice/RetrieveDeviceRouter';
 
 const DiscoveryContainer = new ContainerModule(bind => {
   bind<DiscoverDevicesCase>(DiscoverDevicesCase).toSelf();
-  bind<DiscoverDevicesRouter>(DiscoverDevicesRouter).toSelf();
-  bind<RetrieveDeviceRouter>(RetrieveDeviceRouter).toSelf();
   bind<RetrieveDeviceCase>(RetrieveDeviceCase).toSelf();
 });
 
--- src/modules/Yeelight/YeelightContainer.ts
+++ src/modules/Yeelight/YeelightContainer.ts
@@ -1,14 +1,10 @@
 import { ContainerModule } from 'inversify';
 import ReceiveCommandCase from './ReceiveCommand/ReceiveCommandCase';
-import ReceiveCommandRouter from './ReceiveCommand/ReceiveCommandRouter';
 import RunSceneCase from './RunScene/RunSceneCase';
-import RunSceneRouter from './RunScene/RunSceneRouter';
 
 const YeelightContainer = new ContainerModule(bind => {
   bind<ReceiveCommandCase>(ReceiveCommandCase).toSelf();
-  bind<ReceiveCommandRouter>(ReceiveCommandRouter).toSelf();
   bind<RunSceneCase>(RunSceneCase).toSelf();
-  bind<RunSceneRouter>(RunSceneRouter).toSelf();
 });
 
 export default YeelightContainer;
