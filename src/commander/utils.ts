3c209c14e592e0042107b5a2152123aad04e8957
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-11-24 02:14:22 -0300

    refactor: remove unused code

--- src/commander/utils.ts
+++ src/commander/utils.ts
@@ -1,18 +1,6 @@
-import { AxiosError, AxiosResponse } from 'axios';
-import { IHttpRequest } from '../shared/contracts';
-import { jsonString, logger } from '../shared/Logger';
+import { logger } from '../shared/Logger';
 
 export const ConfigureCmds = (logLevel?: 'verbose' | 'debug' | 'info') => {
   logger.level = logLevel;
 };
 
-export const HandleRequest = async (fn: Promise<AxiosResponse<any>>) => {
-  try {
-    const { data } = await fn;
-    logger.info(jsonString(data));
-  } catch (e) {
-    const error: AxiosError<IHttpRequest<any>> = e;
-    const err = error?.response?.data ? jsonString(error.response?.data) : error.message;
-    logger.error(err);
-  }
-};
