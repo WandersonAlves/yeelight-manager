186c82178418c8eff1ab74d47526db41e74e418f
Wanderson Ferreira <wferreira@questrade.com>
2023-04-24 18:10:04 -0300

    refactor: remove HttpResponse

--- src/shared/decorators/ExceptionHandler.ts
+++ src/shared/decorators/ExceptionHandler.ts
@@ -1,13 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { logger } from '../Logger';
-import GenericException from '../exceptions/GenericException';
-import HttpResponseFactory from '../responses/HttpResponse';
 
 export default function ExceptionHandler(customFn?: (e: Error) => any) {
-  return (target: object, name: string | symbol, descriptor: PropertyDescriptor) => {
+  return (target: unknown, name: unknown, descriptor: PropertyDescriptor) => {
     const original = descriptor.value;
     descriptor.value = async function (...args) {
       try {
@@ -17,16 +14,7 @@ export default function ExceptionHandler(customFn?: (e: Error) => any) {
         if (customFn) {
           return customFn(err);
         }
-        return HttpResponseFactory.error(
-          new GenericException({
-            name: err.name,
-            message: err.message,
-            statusCode: err?.response?.status,
-            extras: {
-              data: err?.response?.data,
-            },
-          }),
-        );
+        process.exit(1);
       }
     };
     return descriptor;
