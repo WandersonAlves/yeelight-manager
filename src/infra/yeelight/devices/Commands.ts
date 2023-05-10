20c5c8f8c0a79e91fd1483a73488089434db5a49
Wanderson Ferreira <wferreira@questrade.com>
2023-05-10 11:15:51 -0300

    fix: expose Command.id

--- src/infra/yeelight/devices/Commands.ts
+++ src/infra/yeelight/devices/Commands.ts
@@ -5,8 +5,13 @@ import ColorFlowExpression from './ColorFlowExpression';
 export type EffectTypes = 'smooth' | 'sudden';
 
 export default class Command {
-  constructor(private id: number, private method: string, private params: any[]) {}
+  constructor(public id: number, private method: string, private params: any[]) {}
 
+  /**
+   * Returns a valid command to be sent to socket/client
+   *
+   * @returns string
+   */
   toString() {
     return JSON.stringify(this) + '\r\n';
   }
