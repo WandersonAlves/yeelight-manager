bde050c329792024a9974c85ea8352ac2ce77629
Wanderson Ferreira <wferreira@questrade.com>
2023-05-13 04:27:11 -0300

    refactor: better code usage on AmbilightCase

--- src/modules/Yeelight/Ambilight/AmbilightCase.ts
+++ src/modules/Yeelight/Ambilight/AmbilightCase.ts
@@ -1,10 +1,12 @@
 import { UseCase } from '../../../shared/contracts';
 import { address } from 'ip';
+import { finishLoop } from '../../../infra/screenshot';
 import { inject, injectable } from 'inversify';
-import { jsonString, logger } from '../../../shared/Logger';
+import { jsonString, labeledLogger } from '../../../shared/Logger';
 import Discovery from '../../../infra/yeelight/discovery/Discovery';
 import ExceptionHandler from '../../../shared/decorators/ExceptionHandler';
 import Screenshot from '../../../infra/screenshot/Screenshot';
+import YeelightDevice from '../../../infra/yeelight/devices/YeelightDevice';
 
 interface AmbilightCaseParams {
   interval?: number;
@@ -20,50 +22,87 @@ interface AmbilightCaseParams {
 export default class AmbilightCase implements UseCase<AmbilightCaseParams, void> {
   @inject(Discovery) private discovery: Discovery;
   private _interval: NodeJS.Timeout;
+  private logger = labeledLogger('ambilightCase');
 
   @ExceptionHandler()
   async execute(params: AmbilightCaseParams): Promise<void> {
     const { deviceNames, interval = 300, width, height, x, y, useLuminance } = params;
-    logger.debug(`Received parameters ${jsonString(params)}`, { label: 'AmbilightCase' });
+
+    this.logger('debug', `Received parameters ${jsonString(params)}`);
+
     const ip = address();
     const devices = await this.discovery.discoverDevices();
+
     if (!devices.length) {
-      logger.error('No devices found', { label: 'ambilightCase' });
+      this.logger('error', 'No devices found');
       return process.exit(0);
     }
-    process.on('SIGINT', () => {
-      selectedDevices.forEach(d => void d.finishMusicMode(ip));
-      clearInterval(this._interval);
-      process.exit(0);
-    });
+
     const selectedDevices = devices.filter(d => deviceNames.includes(d.name));
 
+    this.ListenSignals(selectedDevices, ip);
+
     if (!selectedDevices.length) {
-      logger.error("Specified devices can't be found", { label: 'ambilightCase' });
+      this.logger('error', "Specified devices can't be found");
       return process.exit(0);
     }
 
     await Promise.all(selectedDevices.map(d => d.connect()));
     await this.discovery.turnOnAll(selectedDevices);
     await this.discovery.musicModeAll(ip, selectedDevices);
-    await new Promise(async resolve => {
+
+    await new Promise(async () => {
       this._interval = setInterval(() => {
-        try {
-          const { color, luminance } = Screenshot.FetchPredominantColor(x, y, width, height);
-          const suddenSmooth = interval < 33 ? 'sudden' : 'smooth';
-          selectedDevices.forEach(async d => {
-            if (useLuminance) {
-              void d.setBright(Number(luminance), suddenSmooth, interval);
-            }
-            void d.setHex(color, 'smooth', 200);
-          });
-        } catch (e) {
-          logger.error(e);
-          selectedDevices.forEach(d => void d.finishMusicMode(ip, true));
-          resolve(null);
-          process.exit(1);
-        }
+        const { color, luminance } = Screenshot.FetchPredominantColor(x, y, width, height);
+        this.HandleAmbilightInterval(color, luminance, interval, selectedDevices, useLuminance, ip);
       }, interval);
     });
+  }
+
+  private ListenSignals(selectedDevices: YeelightDevice[], ip: string) {
+    this.logger('debug', 'Adding signal listeners');
+    process.stdin.resume();
+    process.on('SIGINT', () => {
+      this.HandleSignal(selectedDevices, ip);
+    });
+    process.on('SIGQUIT', () => {
+      this.HandleSignal(selectedDevices, ip);
+    });
+    process.on('SIGTERM', () => {
+      this.HandleSignal(selectedDevices, ip);
+    });
+    this.logger('debug', 'Listeners added');
+  }
+
+  private HandleSignal(selectedDevices: YeelightDevice[], ip: string) {
+    finishLoop();
+    selectedDevices.forEach(d => void d.finishMusicMode(ip));
+    clearInterval(this._interval);
+    this.logger('info', '👋🏾 See you soon');
+    process.exit(0);
+  }
+
+  private HandleAmbilightInterval(
+    color: string,
+    luminance: number,
+    interval: number,
+    selectedDevices: YeelightDevice[],
+    useLuminance: boolean,
+    ip: string,
+  ) {
+    try {
+      const suddenSmooth = interval < 33 ? 'sudden' : 'smooth';
+      selectedDevices.forEach(async d => {
+        if (useLuminance) {
+          void d.setBright(Number(luminance), suddenSmooth, interval);
+        }
+        void d.setHex(color, 'smooth', 200);
+      });
+    } catch (e) {
+      const err: Error = e;
+      this.logger('error', err.toString());
+      selectedDevices.forEach(d => void d.finishMusicMode(ip, true));
+      process.exit(1);
+    }
   }
 }
