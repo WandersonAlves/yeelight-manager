8be7376796124ec780672819e425af410541c3cd
Wanderson Ferreira <wferreira@questrade.com>
2023-08-22 17:58:09 -0300

    fix: warn user when rust code is not built

--- src/infra/screenshot/index.js
+++ src/infra/screenshot/index.js
@@ -1,4 +1,10 @@
-const { getDominantColorCallback, getScreenDimensions } = require('./color.node');
+const { logger } = require('../../shared/Logger');
 
-module.exports.getScreenDimensions = getScreenDimensions;
-module.exports.getDominantColorCallback = getDominantColorCallback;
+try {
+  const { getDominantColorCallback, getScreenDimensions } = require('./color.node');
+
+  module.exports.getScreenDimensions = getScreenDimensions;
+  module.exports.getDominantColorCallback = getDominantColorCallback;
+} catch (e) {
+  logger.warn('Rust code not built. To fix this, allow postinstall script to run');
+}
